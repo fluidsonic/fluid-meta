16e87242b6e667eeeac9d8d2238fafa5dc36ede7
Marc Knaup <marc@knaup.io>
2019-02-05 20:16:39 +0100

    updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import com.github.fluidsonic.fluid.library.*
 
 plugins {
-	id("com.github.fluidsonic.fluid-library") version "0.9.0"
+	id("com.github.fluidsonic.fluid-library") version "0.9.1"
 }
 
 fluidLibrary {
