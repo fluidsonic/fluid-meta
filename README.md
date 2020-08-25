a88dd54991688d36849a0cd5e3e4e9b15c93d64d
Marc Knaup <marc@knaup.io>
2020-08-25 13:13:30 +0100

    🎉 0.10.2

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ Installation
 `build.gradle.kts`:
 ```kotlin
 dependencies {
-    implementation("io.fluidsonic.meta:fluid-meta:0.10.1")
+    implementation("io.fluidsonic.meta:fluid-meta:0.10.2")
 }
 ```
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 	id("io.fluidsonic.gradle") version "1.1.8"
 }
 
-fluidLibrary(name = "meta", version = "0.10.1")
+fluidLibrary(name = "meta", version = "0.10.2")
 
 fluidLibraryModule(description = "Converts Kotlin metadata into an easily usable data model") {
 	publishSingleTargetAsModule()
